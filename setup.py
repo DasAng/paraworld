bc5ffe8e9c73747a728864ab1764750ec53e18d6
dasang <>
2021-11-02 20:17:10 +0100

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="paraworld",
       packages=["conclave"],
-      version="0.0.1",
+      version="0.0.2",
       description="A BDD framework for concurrent tests",
       long_description="A BDD framework for concurrent tests",
       author="DasAng",

70c60bbfdee082290e3becae05354072e820f9ac
dasang <>
2021-12-13 13:40:29 +0100

    fixed timeline html crash

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="paraworld",
       packages=["conclave"],
-      version="0.0.14",
+      version="0.0.15",
       description="A BDD framework for concurrent tests",
       long_description="A BDD framework for concurrent tests",
       author="DasAng",

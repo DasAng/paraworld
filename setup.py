b8c4f7de018006fca8281ccb67dfc77e9da38ee0
dasang <>
2022-02-27 20:50:17 +0100

    removed deps on boto3

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name="paraworld",
                    "Programming Language :: Python :: 3",
                    ],
       platforms = ['any'],
-      install_requires=['gherkin-official==22.0.0', 'jinja2==3.0.2','psutil==5.8.0','requests==2.26.0', 'boto3==1.21.7'],
+      install_requires=['gherkin-official==22.0.0', 'jinja2==3.0.2','psutil==5.8.0','requests==2.26.0'],
       include_package_data=True
       )
